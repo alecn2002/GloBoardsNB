e68d29be7d64d0de7f1c0a358fa72adbd3b03563
Alexander Novitskiy <Alexander.Novitskiy@intel.com>
2019-02-27 13:47:40 +0100

    Added Jersey (REST and JSON) dependencies

---
+++ src/main/java/com/alecn/glo/client/BoardsDAO.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alecn.glo.client;
+
+/**
+ *
+ * @author anovitsk
+ */
+public interface BoardsDAO {
+
+}
